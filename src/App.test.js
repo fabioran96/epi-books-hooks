4ca2e01982f98645698c58d25fb70b3d07406f11
Fabio Ranocchiari <fabio.ranocchiari@gmail.com>
2024-07-04 15:28:11 +0200

    update

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,44 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import Welcome from './components/welcome';
+import BookList from './components/BookList';
+import CommentArea from './components/CommentArea'; 
+import scifiBooks from './data/scifi.json'
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+
+
+// Test welcome
+
+test('renders Welcome component', () => {
+  render(<Welcome />);
+  const welcomeElement = screen.getByText(/Welcome To EpiBooks!/i);
+  expect(welcomeElement).toBeInTheDocument();
 });
+
+// Test per le bootstrap cards
+
+test('renders correct numbers of bootstrap cards', () => {
+  render(<BookList books={scifiBooks} onBookSelect={() =>{}}/>);
+  const bookCards = screen.getAllByRole('img');
+  expect(bookCards.lenght).toBe(scifiBooks.lenght);
+})
+
+// Test per il componente CommentArea
+
+test('renders CommentArea component', () => {
+  render(<CommentArea asin = "55667"/>);
+  const commentAreaElement = screen.getByText(/No comments yet/i);
+  expect(commentAreaElement).toBeInTheDocument();
+});
+
+// Test per il filtraggio dei libri
+
+// Test per il cambio di colore del bordo dei libri
+
+// Test per il ripristino del bordo del libro precedente
+
+// Test per l'assenza di SingleComment all'avvio
+
+// Test per il caricamento delle recensioni
+
+
